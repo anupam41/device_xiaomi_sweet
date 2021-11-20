48c8b2c33b01d5b702897a27f308295f65f4b9da
anupam41 <anupamsamanta41@gmail.com>
2021-11-20 16:30:27 +0000

    sweet: octavi bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,7 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_sweet.mk
+    $(LOCAL_DIR)/octavi_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_sweet-eng \
-    arrow_sweet-userdebug \
-    arrow_sweet-user
+    octavi_sweet-eng \
+    octavi_sweet-userdebug \
+    octavi_sweet-user
